1df30f8a4ee9a902e00669d170f4f7362303dc4c
Rohan <rrnarayan1@gmail.com>
2015-10-10 14:04:33 -0700

    With some fucntionality

--- Documents/Mao.py
+++ Documents/Mao.py
@@ -55,33 +55,59 @@ class Deck:
         
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
         self.hand=[]
         self.turn=0
+        self.name=name
         self.is_user=True
 
     def __str__(self):
         blah=""
+        i=0
         for card in self.hand:
-            blah+=str(card)+" "
+            i+=1
+            blah+="("+str(i)+") "+str(card)+" "
         return blah
     
     def __len__(self):
         return len(self.hand)
         
+    def play_card(self,deck):
+        print("Below is player "+self.name+"'s hand")
+        print(self)
+        index_of_wanted_card=int(input("PLAYER "+self.name+":Enter the index of the card you want to play. Enter 0 if you want to draw a card."))
+        if(index_of_wanted_card==0):
+            deck.draw(self)
+            return None
+        wanted_card=self.hand[index_of_wanted_card-1]
+        self.hand.remove(self.hand[index_of_wanted_card-1])
+        return wanted_card
+        
 def playgame(num_players):
     players=[]
     for i in range(num_players):
-        players.append(Player())
+        person_name=input("Enter Your Name Here:")
+        players.append(Player(person_name))
     deck=Deck()
     deck.shuffle_deck()
-    print(deck)
     for player in players:
         if player.turn==0:
             player.hand=deck.create_hand()
-        print(player)
-    #makes sure all players have hands of at least 1
-    while(min([len(player) for player in players] != 0):
+    face_up_card=deck.deck[0]
+    deck.deck.remove(deck.deck[0])
+    #makes sure all players have hands of at least 1 card in their hand
+    while(min([len(player) for player in players]) != 0):
+        for player in players:
+            print("Current Card: "+str(face_up_card))
+            temp_card=player.play_card(deck)
+            if(temp_card==None):
+                face_up_card=face_up_card
+            else:
+                face_up_card=temp_card
+                
+            
+        
+        
         
         
 
